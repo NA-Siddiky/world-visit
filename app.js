ebb41c4a227d81bfa5cc76436a4da53a5a22afe4
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-06 16:53:21 +0600

    adding script

---
+++ app.js
@@ -0,0 +1,61 @@
+fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+
+const displayCountries = countries => {
+    // console.log(countries);      //for checking /
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const countryDiv = document.createElement("div");
+
+        countryDiv.className = "country"
+
+        const countryInfo = `
+        <h3 class = "country-name">${country.name}</h3>
+        <p>${country.capital}</p>
+        `;
+
+        countryDiv.innerHTML = countryInfo;
+        countriesDiv.appendChild(countryDiv);
+    });
+
+
+
+
+
+
+    // for (let i = 0; i < countries.length; i++) {             // 2nd Method /
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement("div");
+
+    //     countryDiv.className= "country"
+
+    //     const countryInfo = `
+    //     <h3 class = "country-name">${country.name}</h3>
+    //     <p>${country.capital}</p>
+
+    //     `;
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
+
+
+
+    // const countryName = document.createElement("h3");        // 1st method /
+    // countryName.innerText = country.name;
+    // countryDiv.appendChild(countryName);        //2nd way/   
+
+    // const countryCapital = document.createElement("p");
+    // countryCapital.innerText = country.capital;
+    // countryDiv.appendChild(countryCapital);     //2nd way/
+
+    // // countryDiv.appendChild(countryName);        //1st way/
+    // // countryDiv.appendChild(countryCapital);     //1st way/
+
+
+    // countriesDiv.appendChild(countryDiv);
+
+    // // countryDiv.innerText = country.name;         //test Case for checking/
+    // // countriesDiv.appendChild(li);
+    // // console.log(country.name);
+}
+// }
